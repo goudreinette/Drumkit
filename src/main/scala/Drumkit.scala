ff9784693d855dc836e3b25bfceb06d44ec6f91a
reinvdwoerd <reinvanderwoerd@me.com>
2018-02-04 15:30:56 +0100

    main class

--- src/main/scala/Drumkit.scala
+++ src/main/scala/Drumkit.scala
@@ -1,7 +1,24 @@
 
-object ScalaFXHelloWorld {
-    
-    def main(args: Array[String]): Unit = {
-        println("Hello Scala")
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Rectangle
+
+object Drumkit extends JFXApp {
+    stage = new JFXApp.PrimaryStage {
+        title.value = "Drumkit"
+        width = 1000
+        height = 700
+        scene = new Scene {
+            fill = LightGreen
+            content = new Rectangle {
+                x = 25
+                y = 40
+                width = 100
+                height = 100
+                fill <== when(hover) choose Green otherwise Red
+            }
+        }
     }
 }
