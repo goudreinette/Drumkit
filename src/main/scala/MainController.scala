857ce05bc26703d0f7e6f42037f34f1ed70a300b
reinvdwoerd <reinvanderwoerd@me.com>
2018-02-05 11:28:03 +0100

    Progress into current measure

--- src/main/scala/MainController.scala
+++ src/main/scala/MainController.scala
@@ -1,24 +1,27 @@
 import javafx.beans.value.{ChangeListener, ObservableValue}
 
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, Label, Slider, ToggleButton}
+import scalafx.scene.control._
 import scalafx.scene.input.MouseDragEvent
 import scalafxml.core.macros.sfxml
 
 
 @sfxml
-class MainController(play: ToggleButton,
-                     model: Model,
+class MainController(model: Model,
+                     play: ToggleButton,
                      secondsLabel: Label,
                      beatMeasureLabel: Label,
                      bpmLabel: Label,
-                     bpmSlider: Slider) {
+                     bpmSlider: Slider,
+                     progress: ProgressBar) {
     
     model.onUpdate(model => {
         secondsLabel.text = f"${model.totalSeconds}%2.2fs"
         beatMeasureLabel.text = f"${model.beatsIntoCurrentMeasure}%2.2f ${model.currentWholeMeasure}"
         bpmLabel.text = s"${model.beatsPerMinute.round}BPM"
         play.selected = model.playing
+        
+        progress.progress = (model.beatsIntoCurrentMeasure / model.beatsInAMeasure)
     })
     
     /**
