706434f0d40282253415e5f4f812d1918bb27ab4
reinvdwoerd <reinvanderwoerd@me.com>
2018-02-06 12:54:08 +0100

    Added docs to quantize

--- src/main/scala/BeatUtils.scala
+++ src/main/scala/BeatUtils.scala
@@ -1,5 +1,6 @@
 object BeatUtils {
     /**
+      * Rounds up or down to the nearest beat-part
       * Where 2 = half, 4 = quarter, etc.
       */
     def quantize(beats: Double, quantizeBy: Int) = {
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -6,10 +6,10 @@ class Tests extends FunSuite {
     val quantizations = Map(
         (3.1234, 2) -> 3,
         (3.125, 4) -> 3.25,
-        (3.1234, 8) -> 3.125,
+        (3.1234, 8) -> 3.125, // Rounding up
 
         (3.9, 2) -> 4,
-        (3.6, 2) -> 3.5
+        (3.6, 2) -> 3.5 // Rounding down
     )
 
     test("quantize should round to the nearest beat-part") {
